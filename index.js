c5d58a3d6101d9b8c65611ab240e79fac824f9c0
P G <pgauchan@gmail.com>
2018-03-08 21:45:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,30 +18,10 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
 	if (string === string.toLowerCase()) {
-    	console.log("I can\'t hear you!")
+    	return "I can\'t hear you!"
 	} else if (string === string.toUpperCase()) {
-    	console.log("YES INDEED!")
+    	return "YES INDEED!"
 	} else if (string === "I love you, Grandma.") {
-    	console.log("I love you, too.")
+    	return "I love you, too."
 	}
 }
-
-
-
-function sayHiToGrandma(string) {
-  if (string === string.toLowerCase) {
-      return "I can\'t hear you!"
-    } 
-  else if (string === string.toUpperCase) {
-    return "YES INDEED";
-    }
-  else if (string === `I love you, Grandma`){
-      return "I love you, too."
-    }
-  }
-
-
-
-
-
-
