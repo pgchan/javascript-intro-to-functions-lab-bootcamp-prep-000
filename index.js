9a19ab80a74242f3f96d500f53da8842da3e0ddb
P G <pgauchan@gmail.com>
2018-03-08 21:20:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -32,15 +32,14 @@ function logWhisper(string) {
 
 
 function sayHiToGrandma(string) {
-  return "I can\'t hear you!" 
-  
   if (string === string.toLowerCase) {
+      return "I can\'t hear you!"
     } 
   else if (string === string.toUpperCase) {
     return "YES INDEED";
     }
-  else {
-      return 
+  else if (string === `I love you, Grandma`){
+      return "I love you, too."
     }
   }
 
